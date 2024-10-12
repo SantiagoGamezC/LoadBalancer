ba1dffcf9caed79dac2201cbacb7987f3edfed05
SantiagoGamezC <santiago.gamez@gmail.com>
2024-10-12 23:24:59 +0000

    Python ip config update, nginx openresty config file

--- pythonipconfg.py
+++ pythonipconfg.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
 
-app = flask(__ipscores__)
+app = Flask(__name__)
 
 ip_scores = {
-        "192.168.4.7": 1,
+        "192.168.7.150": 2
 }
 
 def score_ip(ip_address):
-    retrun ip_scores.get(ip_address, 1)
+    return ip_scores.get(ip_address, 1)
 
-@app.route('/score_ip', methods=[GET])
+@app.route('/score_ip', methods=['GET'])
 def score_ip_route():
     ip_address = request.args.get('ip')
     if not ip_address:
@@ -18,5 +18,5 @@ def score_ip_route():
     score = score_ip(ip_address)
     return jsonify({'ip': ip_address, 'score': score})
 
-if __ipscores__ == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
